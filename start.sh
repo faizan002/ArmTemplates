fa5d1257b890ff2f7559983a489c7ca9d02d8f8c
Faizan <fmud@tobii.com>
2021-01-28 22:39:52 +0100

    add packer build command to launcher script, generate random name for image

--- start.sh
+++ start.sh
@@ -6,15 +6,23 @@ help_text () {
     echo "./start.sh resourceGroupName"
 }
 
+function prepare_image_name () {
+    PREFIX="dev_packer_image"
+    timestamp=$(date +%Y%m%d%h%M%s)
+    IMAGE_NAME=${PREFIX}$timestamp
+}
+
 if [ $PARAM_LENGTH -lt 1 ]
 then
     help_text
     exit 0
 fi
 
-#Create Resource Group
 RG_NAME=$1
 az group create -n $RG_NAME -l northeurope
+
+packer build -var azure_resource_group=$RG_NAME -var azure_image_name=prepare_image_name packer_image.json
+
 
 
 
