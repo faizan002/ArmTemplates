9dc8a22065e2a0ac78141a1e4a5cdbbfd527d476
Faizan <fmud@tobii.com>
2021-01-29 10:12:25 +0100

    Use the variable to set image name in the packer file

--- start.sh
+++ start.sh
@@ -21,7 +21,11 @@ fi
 RG_NAME=$1
 az group create -n $RG_NAME -l northeurope
 
+#Create packer image
 packer build -var azure_resource_group=$RG_NAME -var azure_image_name=prepare_image_name packer_image.json
+
+#Create test VM from the generated packer image
+
 
 
 
